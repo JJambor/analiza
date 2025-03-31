3a8b8390e1bc4d855837f377c876218a241d0f18
Adam <adam91mko@gmail.com>
2025-03-31 18:02:37 +0200

    Add guard

--- new/auth_guard/guard.py
+++ new/auth_guard/guard.py
@@ -1,8 +1,8 @@
 from flask import request, session, redirect, url_for
 from flask_login import LoginManager,current_user, login_manager
 from services.users_service import UsersService
-# routes = ['/dashboard', '/admin']
-routes = []
+routes = ['/dashboard', '/admin']
+# routes = []
 def create_auth_manager(app):
        login_manager = LoginManager()
        login_manager.init_app(app)
