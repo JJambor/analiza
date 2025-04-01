7191f999fa351edfb6ee20786037a43cb0b5b1dc
Adam <adam91mko@gmail.com>
2025-04-01 15:39:00 +0200

    fixes

--- new/bootstrap.py
+++ new/bootstrap.py
@@ -8,16 +8,21 @@ from auth_guard.guard import create_auth_manager, auth
 from config.init_config import load_config
 from redis_client.redis_client import create_redis_client
 
-app = Flask(__name__, template_folder='templates')
-app.before_request(auth)
-load_config(app)
-create_redis_client(app)
-app.register_blueprint(auth_bp, url_prefix='/auth')
-app.register_blueprint(auth_form_bp, url_prefix='/auth')
-app.register_blueprint(home_bp, url_prefix='/')
-app.secret_key = 'tajny-klucz-123'
-create_auth_manager(app)
 
-create_dash(app)
+def get_app():
+    app = Flask(__name__, template_folder='templates')
+    app.before_request(auth)
+    load_config(app)
+    create_redis_client(app)
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+    app.register_blueprint(auth_form_bp, url_prefix='/auth')
+    app.register_blueprint(home_bp, url_prefix='/')
+    app.secret_key = 'tajny-klucz-123'
+    create_auth_manager(app)
+
+    create_dash(app)
+
+    return app
 if __name__ == '__main__':
+    app = get_app()
     app.run(host='0.0.0.0', port=8050, debug=True)
